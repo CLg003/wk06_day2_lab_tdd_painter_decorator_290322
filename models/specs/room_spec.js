d961b35f02e50b4172116e94c886ddf1a7043d36
elicodes89 <elicodes89@gmail.com>
2022-03-29 15:28:54 +0100

    first test failed before passing - room is not painted

--- models/specs/room_spec.js
+++ models/specs/room_spec.js
@@ -5,16 +5,17 @@ describe('Room' , function () {
     let room;
 
 beforeEach(function() {
-    room1 = new Room(5 , false);
-    room2 = new Room(4 , true);
+    room = new Room(5 , false);
 })
 
 it('should have an area' , function (){
-    const actual = room1.area;
+    const actual = room.area;
     assert.strictEqual(actual, 5);
 })
 
-it('should be painted')
+it('should not be painted' , function (){
+    const actual = room.painted;
+    assert.strictEqual(actual, false)
+})
 
-it('should not be painted')
 });
